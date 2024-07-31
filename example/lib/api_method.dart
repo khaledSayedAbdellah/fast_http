0c278bb3062d98888d7855b16c328d1e3375df82
khaledSayedAbdellah <khaled.s.abdellah@gmail.com>
2024-07-31 10:56:44 +0300

    change package name

---
+++ example/lib/api_method.dart
@@ -0,0 +1,30 @@
+import 'dart:typed_data';
+import 'package:fast_http/core/API/request_method.dart';
+import 'package:fast_http/fast_http.dart';
+
+
+class APIMethod{
+  static Future<Either<Failure,dynamic>> executeRequestApi({required RequestApi requestApi})async{
+    try {
+      dynamic response = await GenericRequest<dynamic>(
+        method: requestApi,
+        fromMap: (_)=> _,
+      ).getResponse();
+      return Right(response);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel));
+    }
+  }
+
+  static Future<Either<Failure,Uint8List>> getImageData({required String imagePath})async{
+    try {
+      Uint8List response = await GenericRequest<dynamic>(
+        method: RequestApi.get(url: imagePath,),
+        fromMap: emptyFromMap,
+      ).getBytes();
+      return Right(response);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel));
+    }
+  }
+}
